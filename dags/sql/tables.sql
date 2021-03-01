641420a77f4bef7237a7cb138113388236384f49
D-Bits <lockwood.da88@gmail.com>
2021-03-01 12:50:21 -0800

    Cleaned up fact_orders table.

--- dags/sql/tables.sql
+++ dags/sql/tables.sql
@@ -1,12 +1,40 @@
-/*
-* Queries to build tables for Northwind 
+/**
+* Queries to build tables for a Northwind 
 * data warehouse.
-*/
+**/
 
 
--- 
+-- Fact table for order data 
 CREATE TABLE fact_orders
 (
-	id SERIAL,
-	
+	-- From orders table
+	id SERIAL PRIMARY KEY,
+	order_id INT NOT NULL,
+	customer_id INT NOT NULL,
+	employeeid INT NOT NULL,
+	productid INT NOT NULL,
+	requireddate DATE NOT NULL,
+	orderdate DATE NOT NULL,
+	shippeddate DATE NOT NULL, 
+	shipvia INT,
+	freight INT,
+	shipname VARCHAR(255) NOT NULL,
+	shipaddress VARCHAR(255) NOT NULL,
+	shipcity VARCHAR(255) NOT NULL,
+	shipregion VARCHAR(4) NOT NULL,
+	shippostalcode VARCHAR(10) NOT NULL,
+	shipcountry VARCHAR(15) NOT NULL,
+	-- From order_details table
+	unitprice FLOAT NOT NULL,
+	quantity INT NOT NULL,
+	discount INT,
+);
+
+-- Fact table for product data
+CREATE TABLE fact_products
+(
+	id SERIAL PRIMARY KEY,
+	unitsinstock INT NOT NULL,
+	unitsinorder INT NOT NULL,
+	reorderlevel INT
 );
