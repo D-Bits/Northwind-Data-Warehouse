93519fd4fc5de60b48cab3f764cd5df05f0ae3e2
D-Bits <lockwood.da88@gmail.com>
2021-03-01 23:58:06 -0800

    Changed order_fact_etl to seed_order_fact

---
+++ dags/seed_order_fact.py
@@ -0,0 +1,60 @@
+"""
+ETL pipeline to seed the order_fact table.
+"""
+from airflow.decorators import dag, task
+from datetime import datetime
+from credentials import oltp_engine, olap_engine
+import pandas as pd 
+
+
+default_args = {
+    "owner": "airflow",
+    "start_date": datetime(2021, 2, 21),
+    "schedule_interval": "@daily",
+    "retries": 1,
+}
+
+
+@dag(default_args=default_args, start_date=None)
+def seed_order_fact():
+
+    # Extract tables from the OLTP db and return a list of dataframes.
+    @task()
+    def extract() -> list:
+
+        # Load "orders" table into DataFrame
+        orders_df = pd.read_sql_table("orders", oltp_engine, index_col="orderid")
+        # Load "order_details" table into DataFrame
+        details_df = pd.read_sql_table("order_details", oltp_engine, index_col="orderid")
+
+        return list([orders_df, details_df])
+
+
+    # Clean dataframes, and merge them into one df
+    @task()
+    def transform(df_list: list):
+
+        merged_df = pd.concat(df_list)
+
+        return merged_df
+
+    
+    @task()
+    def load(cleaned_df: pd.DataFrame):
+
+        cleaned_df.to_sql(
+            'fact_orders', 
+            olap_engine, 
+            index=False, 
+            method='multi', 
+            if_exists='append',
+        )
+
+
+    
+    extract = extract()
+    transform = transform(extract)
+    load = load(transform)
+
+
+seed_order_fact = seed_order_fact()
